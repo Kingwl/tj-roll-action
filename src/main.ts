476f562dd6da0a8908189c21eedca4c1956b1bfb
Wenlu Wang <wenlwan@microsoft.com>
2020-10-27 17:10:41 +0800

    Add main process

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,84 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import axios from 'axios'
+import * as qs from 'querystring'
+import * as cherrio from 'cheerio'
+
+const url = 'http://apply.xkctk.jtys.tj.gov.cn/apply/norm/personQuery.html'
+
+async function runRequest(issueNumber: string, applyCode: string) {
+  const options = qs.stringify({
+    pageNo: 1,
+    issueNumber,
+    applyCode
+  })
+
+  return axios.post(url, options)
+}
+
+enum ResultType {
+  Success,
+  Failed
+}
+
+interface SuccessResult {
+  type: ResultType.Success
+  name: string
+}
+
+interface FailedResult {
+  type: ResultType.Failed
+}
+
+type Result = SuccessResult | FailedResult
+
+function parseResult(content: string): Result {
+  const doc = cherrio.load(content)
+  const allImages = doc('img')
+  const allSrc = allImages.map((_, img) => doc(img).attr('src'))
+  const allSrcRaw: string[] = allSrc.get()
+  const hasFailedResult = allSrcRaw
+    .map(x => x.toLowerCase())
+    .some(x => x.endsWith('notballot.png'))
+  if (hasFailedResult) {
+    return {
+      type: ResultType.Failed
+    }
+  }
+
+  const resultTd = doc('.main_content_bg > table table.ge2_content td')
+  const name = (<string>resultTd.map((_, x) => doc(x).text()).get(1)).trim()
+  return {
+    type: ResultType.Success,
+    name
+  }
+}
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const applyCode = core.getInput('apply-code')
+    const issueNumber = core.getInput('issue-number')
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    console.log(`Apply code is: ${applyCode}`)
+    console.log(`Issue number is: ${issueNumber}`)
+    console.log(`Let's check!`)
 
-    core.setOutput('time', new Date().toTimeString())
+    const resp = await runRequest(applyCode, issueNumber)
+    console.log(`Request succeed`)
+    console.log('Parse start')
+    const result = parseResult(resp.data)
+
+    if (result.type === ResultType.Failed) {
+      console.log('Sorry, You did not win.')
+
+      core.setOutput('result', false)
+      core.setOutput('name', '')
+    } else {
+      console.log('Holy shit! You have win the roll!')
+      core.setOutput('result', true)
+      core.setOutput('name', result.name)
+    }
+
+    console.log('All done!')
   } catch (error) {
     core.setFailed(error.message)
   }
